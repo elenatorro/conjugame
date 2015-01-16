ce571bb44fbd9b3153d7e085b16622588e25e8a9
Elena Torro <elenatorro@gmail.com>
2015-01-16 10:03:29 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+/********* Dependencies *********/
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var path = require('path');
+
+/************* Tasks *************/
+gulp.task('looklib', function() {
+  return gulp.src('lib/Look.js')
+        .pipe(uglify())
+        .pipe(rename('Look.min.js'))
+        .pipe(gulp.dest('lib'));
+})
+
+gulp.task('buildlook', function() {
+  return gulp.src(['lib/look.dom.js','lib/look.language.js'])
+  .pipe(concat('Look.js'))
+  .pipe(gulp.dest('lib'));
+})
+
+
+gulp.task('default', ['looklib']);
---
+++ test/qunit_tests.js
@@ -0,0 +1,75 @@
+/*############## TEST FUNCTIONS ##############*/
+
+/*Div Creation*/
+function testDivCreation(object, container) {
+  QUnit.test(object, function(assert) {
+    var div = Look.create(object, container);
+    assert.equal(container.lastChild, div)
+  })
+}
+testDivCreation({'tag':'div'}, document.getElementById('container_one'));
+
+/*Div Creation by Default*/
+function testDivCreationDefault(object) {
+  QUnit.test(object, function(assert) {
+    var div = Look.create(object);
+    assert.equal(document.body.lastChild, div)
+  })
+}
+testDivCreationDefault({'tag':'div'});
+
+
+/*Div Parameters Id and Class*/
+function testDivParameters(object, container) {
+  var div = Look.create(object,container);
+  QUnit.test(object, function(assert) {
+    assert.equal('divClass', div.className);
+  });
+
+  QUnit.test(object, function(assert) {
+    assert.equal('divId', div.id);
+  });
+}
+testDivParameters({'tag':'div', 'attr' : {'class':'divClass', 'id':'divId'}}, document.getElementById('container_two'));
+
+function testDivRecursion(object, container) {
+  var div = Look.create(object,container);
+  QUnit.test(object, function(assert) {
+    assert.equal('firstSon', div.firstChild.id);
+  });
+
+  QUnit.test(object, function(assert) {
+    assert.equal('secondSon', div.lastChild.id);
+  });
+}
+
+testDivRecursion({
+  'tag'   :'div',
+   'attr' : {
+      'class' : 'divClass',
+      'id'    : 'divId2'
+      },
+
+    'content' : [{
+      'tag'   : 'div',
+      'attr'  : {
+        'id'  : 'firstSon'
+        }
+      },{
+        'tag'   : 'div',
+        'attr'  : {
+          'id'  : 'secondSon'
+        }
+      }]
+      },
+    document.getElementById('container_three')
+  )
+
+  function testDivCopy(copyFrom, copyTo) {
+    var div = Look.copy(copyFrom, copyTo);
+    QUnit.test(object, function(assert) {
+      assert.equal(div, div.lastChild);
+    });
+  }
+
+  testDivCreationDefault('container_three', 'container_four');
