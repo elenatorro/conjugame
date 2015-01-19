c0d7b22630cd9bbb1c4c759daee66980c069b8f1
Elena Torro <elenatorro@gmail.com>
2015-01-19 09:23:40 +0100

    new tests written

--- test/qunit_tests.js
+++ test/qunit_tests.js
@@ -1,10 +1,47 @@
 /*############## TEST FUNCTIONS ##############*/
 
-/*Div Creation*/
-// function testDivCreation(object, container) {
-//   QUnit.test(object, function(assert) {
-//     var div = Look.create(object, container);
-//     assert.equal(container.lastChild, div)
-//   })
-// }
-// testDivCreation({'tag':'div'}, document.getElementById('container_one'));
+/*Saber conjugacion*/
+function testConjugation(verbo) {
+  QUnit.test(verbo, function(assert) {
+    var conj = c.conjugacion(verbo);
+    console.log(conj);
+    assert.equal(conj, 'ar')
+  })
+}
+testConjugation('cantar');
+
+function testConjugation2(verbo) {
+  QUnit.test(verbo, function(assert) {
+    var conj = c.conjugacion(verbo);
+    console.log(conj);
+    assert.equal(conj, 'er')
+  })
+}
+testConjugation2('temer');
+
+function testRaiz(verbo) {
+  QUnit.test(verbo, function(assert) {
+    var raiz = c.raiz(verbo);
+    assert.equal(raiz, 'cant')
+  })
+}
+
+testRaiz('cantar');
+
+
+function testBuscarRaiz(cadena, conjugacion) {
+  QUnit.test([cadena, conjugacion], function(assert) {
+    var raiz = c.conjugar(cadena, conjugacion);
+    assert.equal(raiz, '1#presente#singular')
+  })
+}
+testBuscarRaiz('o','ar');
+
+function testBuscarRaiz2(cadena, conjugacion) {
+  QUnit.test(cadena, function(assert) {
+    var raiz = c.conjugar(cadena, 'ir');
+    assert.equal(raiz, '2#condicional|simple#plural')
+  })
+}
+
+testBuscarRaiz2('partiríais','ir');
